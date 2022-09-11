5dccab075aa22ec0c6fc9891d3891efdf4236da5
Urkellitow <Shkelqimcakaj@hotmail.com>
2022-09-12 00:26:46 +0200

    added some rnadom text too the home page and also buttons that redirects to the other pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    let navigate = useNavigate();
     return (
         <div className='homeContainer'>
             <h2>Welcome!</h2>
-            <div>
+            <div className='welcome'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at, pariatur sunt libero est quam. Molestiae dolor ratione dolorum dicta voluptatibus commodi quisquam libero itaque voluptate veniam soluta, quod temporibus.
+            </div>
+            <div className='homeBtns'>
+                <button onClick={() => navigate("/Todos")} >CREATE TODOS</button>
+                <button onClick={() => navigate("/DadJokes")}>FETCH DAD JOKES</button>
             </div>
 
 
